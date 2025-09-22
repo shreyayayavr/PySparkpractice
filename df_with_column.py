bba1748430c868cabeb8f4eb8509bdca24493622
Shreya VR <shreyarangachar@gmail.com>
2025-09-23 02:53:19 +0530

    chore:new files

---
+++ df_with_column.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder.master("local").appName("SparkPractice").getOrCreate()
+
+# Create an RDD of tuples
+rdd = spark.sparkContext.parallelize([(1,), (2,), (3,), (4,), (5,)])
+columns = ["num"]
+
+# Convert to DataFrame
+rdd_to_df = rdd.toDF(columns)
+
+# Add a new column with square
+df_with_square = rdd_to_df.withColumn("num_square", col("num") * col("num"))
+
+df_with_square.show()
+
+
